2b76489a6982838698b2111da95f119c5515678b
Varshithvhegde <varshithvh@gmail.com>
2022-07-31 14:38:34 +0530

    Added Reason why styl is not applied

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ fetch('https://script.google.com/macros/s/AKfycbyGrtQOUbZJpfJYws3G40Awek7Kz-OZ7a
    .then(characters => showCharacters(characters.data));
    showCharacters = characters => {
    
-    document.write("<table><tr><th>Name</th><th>USN</th><th>");
+    document.write("<table><tr><th><h2>Name</h2></th><th><h2>USN</h2></th><th>");
     characters.forEach(character => {
         if(character.Name!="Name"){
+            if(character.USN=="4MT19CS174"){
+                document.write("<tr style='color:red;font-weight: bold;'><td>" + character.Name + "</td><td>" + character.USN + "</td><td>");
+            }
+            
     //   const characterElement = document.createElement('p');
     //   const USN = document.createElement('p');
-      document.write("<tr><td>" + character.Name + "</td><td>" + character.USN + "</td><td>");
+    else{
+      document.write("<tr style='color:black;font-weight: bold;'><td>" + character.Name + "</td><td>" + character.USN + "</td><td>");
      // characterElement.innerText = `Name: ${character.Name}`;
     //     USN.innerText = `USN: ${character.USN}`;
     //   charactersDiv.append(characterElement);
     //     charactersDiv.append(USN);
+    }
         }
     });
 }
